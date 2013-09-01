22a4ca5f85a270e9d5199a4aa7b7e28010f8c984
atkinchris <atkinchris@gmail.com>
2013-09-01 11:44:50 +0100

    Added demo array and colours

--- slyd-desktop/src/uk/co/slyd/Main.java
+++ slyd-desktop/src/uk/co/slyd/Main.java
@@ -4,13 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
+
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "slyd";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
-		
+		cfg.width = 320;
+		cfg.height = 480;
+
 		new LwjglApplication(new slyd(), cfg);
 	}
 }
--- slyd/src/uk/co/slyd/slyd.java
+++ slyd/src/uk/co/slyd/slyd.java
@@ -2,53 +2,74 @@ package uk.co.slyd;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class slyd implements ApplicationListener {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
-	
+
+	private OrthographicCamera	camera;
+	private SpriteBatch			batch;
+	private Texture[]			textures;
+	private Integer[][]			grid;
+
+	public static final String	colour1	= "69D2E7";
+	public static final String	colour2	= "A7DBD8";
+	public static final String	colour3	= "E0E4CC";
+	public static final String	colour4	= "F38630";
+	public static final String	colour5	= "FA6900";
+
 	@Override
-	public void create() {		
+	public void create() {
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
+
+		grid = new Integer[][] { { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 } };
+
+		camera = new OrthographicCamera(1, h / w);
 		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+
+		Pixmap pixmap = new Pixmap(1, 1, Format.RGBA8888);
+
+		pixmap.setColor(Color.valueOf(colour3));
+		pixmap.fill();
+		textures[0] = new Texture(pixmap);
+
+		pixmap.setColor(Color.valueOf(colour2));
+		pixmap.fill();
+		textures[1] = new Texture(pixmap);
+
+		pixmap.dispose();
+
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
-		texture.dispose();
+		for (int i = 0; i < textures.length; i++) {
+			textures[i].dispose();
+		}
 	}
 
 	@Override
-	public void render() {		
+	public void render() {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
+
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		sprite.draw(batch);
+
+		for (int x = 0; x < grid.length; x++) {
+			for (int y = 0; y < grid.length; y++) {
+				Gdx.app.log("Grid", x + "," + y + "-" + grid[x][y]);
+			}
+		}
+
 		batch.end();
 	}
 
